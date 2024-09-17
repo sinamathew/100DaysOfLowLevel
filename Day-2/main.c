ac240f53b02ea4030154bfaacfa540528c7a3ec3
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-09-17 22:52:18 +0100

    Main file to test funtions

---
+++ Day-2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "header.h"
+
+/**
+ * main - Entry point
+ * Description: print out string with _printchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *str = "Hello, Alkebulan!\n";
+
+	_printchar(str);
+	return (0);
+}
