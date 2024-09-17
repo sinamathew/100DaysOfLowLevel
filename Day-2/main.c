2a8ae331ae9a50ccd3864d9810707b3488140858
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-09-18 00:01:46 +0100

    Simple guesing game in C

--- Day-2/main.c
+++ Day-2/main.c
@@ -1,16 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "header.h"
 
 /**
  * main - Entry point
- * Description: print out string with _printchar
+ * Description: a simple guessing game
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char *str = "Hello, Alkebulan!\n";
+	int number, attempts, predict;
+	char *greetings, *rule1, *rule2, *rule3, *rule4;
 
-	_printchar(str);
+	srand(time(NULL));
+	greetings = "Hello, Alkebulan!\nWelcome to the Game of Life!\n\n";
+	rule1 = "\t1. You have three lives.\n";
+	rule2 = "\t2. You have to guess a number between 0 and 5.\n";
+	rule3 = "\t3. If you guess the number correctly, you live.\n";
+	rule4 = "\t4. If you guess the number incorrectly, you lose a life.\n";
+	number = rand() % 5;
+	attempts = 3;
+
+	_printchar(greetings);
+	_printchar(rule1);
+	_printchar(rule2);
+	_printchar(rule3);
+	_printchar(rule4);
+	_printchar("Enter a number: ");
+
+	while (attempts)
+	{
+		scanf("%d", &predict);
+		if (predict == number)
+		{
+			_printchar("You live!\n");
+			break;
+		}
+		else if (predict != number && attempts != 1)
+		{
+			_printchar("You just lost a live!\nTry again: ");
+			attempts--;
+		}
+		else
+		{
+			_printchar("You are dead!\nGame over!\n");
+			break;
+		}
+	}
+
 	return (0);
 }
